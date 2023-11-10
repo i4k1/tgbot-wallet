8aaf6916b5818e288e894565d7ba0827565aae3f
i4k1 <99640421+i4k1@users.noreply.github.com>
2023-11-11 01:26:33 +0400

    minor changes

--- funcs.js
+++ funcs.js
@@ -12,41 +12,63 @@ const abi = [ "function transfer(address to, uint amount)",
 const contractp = new Contract(walletJSON.ERC20, abi, provider); // ERC20 token contract address
 const contractw = new Contract(walletJSON.ERC20, abi, wallet);
 
-async function getBalance() {
-    const balance = await provider.getBalance(wallet.address);
-    const balanceInEth = ethers.formatEther(balance); // wei to ether
-    return balanceInEth;
+async function getBalanceETH(value) {
+    if ( value === "amount" ) {
+        const balance = await provider.getBalance(wallet.address);
+        const balanceInEth = ethers.formatEther(balance); // wei to ether
+        return balanceInEth;
+    } else if ( value === "name" ) {
+        const networkDetect = await provider._detectNetwork();
+        const networkName = networkDetect["name"];
+        return networkName;
+    }
 }
 
-async function sendEth(whomTransfer, numOfTokens) {
-    const tx = await wallet.sendTransaction({ to: whomTransfer, value: parseEther(numOfTokens) });
-    const receipt = await tx.wait();
-    console.log(tx.hash);
-    console.log(receipt);
+async function getBalanceERC20(value) {
+    if ( value === "amount" ) {
+        const balance = await contractp.balanceOf(wallet.address);
+        const balanceInEth = ethers.formatEther(balance); // wei to ether
+        return balanceInEth;
+    } else if ( value === "name" ) {
+        const erc20_name = await contractp.name();
+        return erc20_name;
+    } else if ( value === "symbol" ) {
+        const erc20_symbol = await contractp.symbol();
+        return erc20_symbol;
+    }
 }
 
-async function getERC20() {
-    const balance = await contractp.balanceOf(wallet.address);
-    const balanceInEth = ethers.formatEther(balance); // wei to ether
-    return balanceInEth;
-}
+async function sendEth(whomTransfer, amount) {
+    // const balance = await contractp.balanceOf(wallet.address);
+    // const balanceInEth = ethers.formatEther(balance); // wei to ether
 
-async function getERC20Name() {
-    const erc20_name = await contractp.name();
-    return erc20_name;
-}
+    const balance = await provider.getBalance(wallet.address)
+    let sendValue = parseEther(amount)
+    let str = ethers.formatEther(sendValue)
 
-async function getERC20Symbol() {
-    const erc20_symbol = await contractp.symbol();
-    return erc20_symbol;
+    if ( balance >= sendValue ) {
+        const tx = await wallet.sendTransaction({ to: whomTransfer, value: parseEther(str) });
+        const receipt = await tx.wait();
+        console.log(tx.hash);
+        console.log(receipt);
+        return tx.hash;
+    } else {
+        return "insufficient funds";
+    }
 }
 
 async function sendERC20(whomTransfer, numOfTokens) {
+    const balance = await contractw.balanceOf(wallet.address);
     const amount = parseUnits(numOfTokens, 18); // number of tokens
-    const tx = await contractw.transfer(whomTransfer, amount); // to whom to transfer
-    const receipt = await tx.wait();
-    console.log(tx.hash);
-    console.log(receipt);
+    if ( balance >= amount ) {
+        const tx = await contractw.transfer(whomTransfer, amount); // to whom to transfer
+        const receipt = await tx.wait();
+        console.log(tx.hash);
+        console.log(receipt);
+        return tx.hash;
+    } else {
+        return "insufficient funds";
+    }
 }
 
-module.exports = { getBalance, getERC20, getERC20Name, getERC20Symbol };
+module.exports = { getBalanceETH, getBalanceERC20, sendEth, sendERC20 };
