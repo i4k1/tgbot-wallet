e12e72f046c9d5519b06741c53aea65dd9c47d71
i4k1 <99640421+i4k1@users.noreply.github.com>
2023-11-12 23:01:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 12-11-2023 i4k
 
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 const ethPrice = require("eth-price");
 const funcs = require("./funcs.js");
 const walletJSON = require("./wallet.json");
@@ -15,10 +15,31 @@ bot.command ("ethbalance", async (ctx) => {
 });
 
 bot.command ("erc20balance", async (ctx) => {
-    let get_balance_erc20 = await funcs.getBalanceERC20("amount");
-    let get_erc20_name = await funcs.getBalanceERC20("name");
-    let get_erc20_symbol = await funcs.getBalanceERC20("symbol");
-    ctx.replyWithHTML(`${get_balance_erc20} <b>${get_erc20_symbol}</b> <i>${get_erc20_name}</i>`);
+    let get_chain_id = await funcs.getBalanceETH("id");
+    if ( get_chain_id === 97n ) {
+        let tokens = [];
+        for ( let i = 0; i < walletJSON.ERC20tokens.length; i++ ) {
+            let get_erc20_amount = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "amount");
+            let get_erc20_symbol = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "symbol");
+            let get_erc20_name = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "name");
+            ctx.replyWithHTML(`${get_erc20_amount} <i><b>${get_erc20_symbol}</b></i> (<i>${get_erc20_name}</i>)\n<code>${walletJSON.ERC20tokens[i]}</code>\nhttps://testnet.bscscan.com/tx/${walletJSON.ERC20tokens[i]}`);
+        }
+    } else if ( get_chain_id === 80001n ) {
+        for ( let i = 0; i < walletJSON.ERC20tokens.length; i++ ) {
+            let get_erc20_amount = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "amount");
+            let get_erc20_symbol = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "symbol");
+            let get_erc20_name = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "name");
+            ctx.replyWithHTML(`${get_erc20_amount} <i><b>${get_erc20_symbol}</b></i> (<i>${get_erc20_name}</i>)\n<code>${walletJSON.ERC20tokens[i]}</code>\nhttps://mumbai.polygonscan.com/tx/${walletJSON.ERC20tokens[i]}`);
+        }
+    } else {
+        let tokens = [];
+        for ( let i = 0; i < walletJSON.ERC20tokens.length; i++ ) {
+            let get_erc20_amount = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "amount");
+            let get_erc20_symbol = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "symbol");
+            let get_erc20_name = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "name");
+            ctx.replyWithHTML(`${get_erc20_amount} <i><b>${get_erc20_symbol}</b></i> (<i>${get_erc20_name}</i>)\n<code>${walletJSON.ERC20tokens[i]}</code>`);
+        }
+    }
 });
 
 bot.command ("ethprice", async (ctx) => {
@@ -44,6 +65,8 @@ bot.command ("sendeth", async (ctx) => {
 
                 if ( get_chain_id === 97n ) {
                     ctx.replyWithHTML(`https://testnet.bscscan.com/tx/${send_eth}`);
+                } else if ( get_chain_id === 80001n ) {
+                    ctx.replyWithHTML(`https://mumbai.polygonscan.com/tx/${send_eth}`);
                 } else {
                     ctx.replyWithHTML(`<code>${send_eth}</code>`);
                 }
@@ -54,31 +77,44 @@ bot.command ("sendeth", async (ctx) => {
     });
 });
 
+bot.command ("erc20aviable", async (ctx) => {
+    let available_tokens = [];
+    for ( let i = 0; i < walletJSON.ERC20tokens.length; i++ ) {
+        let get_erc20_symbol = await funcs.getBalanceERC20(walletJSON.ERC20tokens[i].toString(), "symbol");
+        available_tokens.push(`<code>${walletJSON.ERC20tokens[i]}</code> <i><b>${get_erc20_symbol}</b></i>`);
+    }
+
+    ctx.replyWithHTML(`Доступны следующие токены:\n${available_tokens}`);
+});
+
 bot.command ("senderc20", async (ctx) => {
-    let get_address, get_amount;
-    ctx.reply("Кому?");
+    ctx.replyWithHTML("Отправь мне сообщение следующего формата:\n\nадресКонтрактаТокена адресКомуПереводите колвоТокенов");
+    bot.on ("text", async (ctx) => {
+        const get_form_message = ctx.message.text.split(" ");
 
-    bot.hears(/^0x\w+/, async (ctx) => {
-        get_address = ctx.message.text;
-        ctx.reply("Сколько?");
-
-        bot.hears(/^\d+/, async (ctx) => { // what the fuck?
-            get_amount = ctx.message.text;
-            let get_balance_erc20 = await funcs.getBalanceERC20("amount");
+        if ( get_form_message.length === 3 && get_form_message[0].startsWith("0x") && get_form_message[1].startsWith("0x") && !isNaN(get_form_message[2]) ) {
+            const get_sent_token = get_form_message[0];
+            const get_address = get_form_message[1];
+            const get_amount = get_form_message[2];
+            const get_balance_erc20 = await funcs.getBalanceERC20(get_sent_token, "amount");
 
             if ( get_balance_erc20 >= get_amount ) {
                 let get_chain_id = await funcs.getBalanceETH("id");
-                let send_erc20 = await funcs.sendERC20(get_address, get_amount);
+                let send_erc20 = await funcs.sendERC20(get_sent_token, get_address, get_amount);
 
                 if ( get_chain_id === 97n ) {
                     ctx.replyWithHTML(`https://testnet.bscscan.com/tx/${send_erc20}`);
+                } else if ( get_chain_id === 80001n ) {
+                    ctx.replyWithHTML(`https://mumbai.polygonscan.com/tx/${send_eth}`);
                 } else {
                     ctx.replyWithHTML(`<code>${send_erc20}</code>`);
                 }
             } else {
                 ctx.replyWithHTML("Недостаточно средств");
             }
-        });
+        } else {
+            ctx.reply("error");
+        }
     });
 });
 
