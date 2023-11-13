72a90fe805fecaf7ab2a739868849ea13bdc23b0
i4k1 <99640421+i4k1@users.noreply.github.com>
2023-11-14 00:32:27 +0400

    minor fixes, nothing to see here

--- funcs.js
+++ funcs.js
@@ -1,11 +1,11 @@
-// 12-11-2023 i4k
+// 14-11-2023 i4k
 
 const ethers = require("ethers");
 const { parseUnits, parseEther, Contract } = require("ethers");
 const walletJSON = require("./wallet.json");
 
-const provider = new ethers.JsonRpcProvider(walletJSON.nodeUrl); // blockchain RPC node link
-const wallet = new ethers.Wallet(walletJSON.privateKey, provider); // wallet private key
+// const provider = new ethers.JsonRpcProvider(walletJSON.nodeUrl); // blockchain RPC node link
+// const wallet = new ethers.Wallet(walletJSON.privateKey, provider); // wallet private key
 const abi = [
     "function transfer(address to, uint amount)",
     "function balanceOf(address) public view returns (uint)",
@@ -13,7 +13,10 @@ const abi = [
     "function name() view returns (string)"
 ];
 
-async function getBalanceETH(value) {
+async function getBalanceETH(node_url, value) {
+    const provider = new ethers.JsonRpcProvider(node_url); // blockchain RPC node link
+    const wallet = new ethers.Wallet(walletJSON.privateKey, provider); // wallet private key
+
     if ( value === "amount" ) {
         const balance = await provider.getBalance(wallet.address);
         const balanceInEth = ethers.formatEther(balance); // wei to ether
@@ -31,7 +34,9 @@ async function getBalanceETH(value) {
     }
 }
 
-async function getBalanceERC20(token, value) {
+async function getBalanceERC20(node_url, token, value) {
+    const provider = new ethers.JsonRpcProvider(node_url); // blockchain RPC node link
+    const wallet = new ethers.Wallet(walletJSON.privateKey, provider); // wallet private key
     const contract = new Contract(token, abi, provider); // ERC20 token contract address
     if ( value === "amount" ) {
         const balance = await contract.balanceOf(wallet.address);
@@ -48,14 +53,18 @@ async function getBalanceERC20(token, value) {
     }
 }
 
-async function sendETH(addressTo, amount) {
+async function sendETH(node_url, addressTo, amount) {
+    const provider = new ethers.JsonRpcProvider(node_url); // blockchain RPC node link
+    const wallet = new ethers.Wallet(walletJSON.privateKey, provider); // wallet private key
     const tx = await wallet.sendTransaction({ to: addressTo, value: parseEther(amount) });
     const receipt = await tx.wait();
     console.log(tx.hash, receipt);
     return tx.hash;
 }
 
-async function sendERC20(token, addressTo, amountToken) {
+async function sendERC20(node_url, token, addressTo, amountToken) {
+    const provider = new ethers.JsonRpcProvider(node_url); // blockchain RPC node link
+    const wallet = new ethers.Wallet(walletJSON.privateKey, provider); // wallet private key
     const contract = new Contract(token, abi, wallet);
     const amount = parseUnits(amountToken, 18); // number of tokens
     const tx = await contract.transfer(addressTo, amount); // to whom to transfer
